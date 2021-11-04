d9da5764c71eb9b1627615257a160a123c485e4d
Hasan Gürsoy <hasan@robomotion.io>
2021-11-04 11:31:52 +0300

    credentials and spec update

--- runtime/credential.go
+++ runtime/credential.go
@@ -1,16 +1,47 @@
 package runtime
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/mitchellh/mapstructure"
+	"github.com/robomotionio/robomotion-go/message"
+)
 
 type Credential struct {
+	Scope string `json:"scope"`
+	Name  string `json:"name"`
+
+	// Deprecated
+	VaultID string `json:"vaultId,omitempty"`
+	// Deprecated
+	ItemID string `json:"itemId,omitempty"`
+}
+
+type credential struct {
 	VaultID string `json:"vaultId"`
 	ItemID  string `json:"itemId"`
 }
 
-func (c *Credential) Get() (map[string]interface{}, error) {
+func (c *Credential) Get(ctx message.Context) (map[string]interface{}, error) {
 	if client == nil {
 		return nil, fmt.Errorf("Runtime was not initialized")
 	}
 
-	return client.GetVaultItem(c.VaultID, c.ItemID)
+	if c.VaultID != "" && c.ItemID != "" {
+		return client.GetVaultItem(c.VaultID, c.ItemID)
+	}
+
+	v := &InVariable{Variable: Variable{Scope: c.Scope, Name: c.Name}}
+	cr, err := v.Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	var creds credential
+	err = mapstructure.Decode(cr, &creds)
+	if err != nil {
+		return nil, err
+	}
+
+	return client.GetVaultItem(creds.VaultID, creds.ItemID)
 }
