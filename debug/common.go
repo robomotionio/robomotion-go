94e3b7cab31b301b2d721524ca0304c5aadcd210
Hasan Gürsoy <hasan@robomotion.io>
2022-04-12 14:38:19 +0300

    os specific netstat

--- debug/common.go
+++ debug/common.go
@@ -4,11 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"os/exec"
 	"reflect"
-	"regexp"
-	"strconv"
-	"strings"
 
 	gops "github.com/mitchellh/go-ps"
 	"github.com/robomotionio/robomotion-go/proto"
@@ -82,22 +78,6 @@ type SockTabEntry struct {
 	LocalAddress string
 }
 
-const (
-	SS_UNKNOWN     SocketState = "UNKNOWN"
-	SS_CLOSED      SocketState = ""
-	SS_LISTENING   SocketState = "LISTENING"
-	SS_SYN_SENT    SocketState = "SYN_SENT"
-	SS_SYN_RECV    SocketState = "SYN_RECV"
-	SS_ESTABLISHED SocketState = "ESTABLISHED"
-	SS_FIN_WAIT1   SocketState = "FIN_WAIT1"
-	SS_FIN_WAIT2   SocketState = "FIN_WAIT2"
-	SS_CLOSE_WAIT  SocketState = "CLOSE_WAIT"
-	SS_CLOSING     SocketState = "CLOSING"
-	SS_LAST_ACK    SocketState = "LAST_ACK"
-	SS_TIME_WAIT   SocketState = "TIME_WAIT"
-	SS_DELETE_TCB  SocketState = "DELETE_TCB"
-)
-
 func NewList(d interface{}) *List {
 	arrType := reflect.TypeOf(d)
 	kind := arrType.Kind()
@@ -130,64 +110,4 @@ func (l *List) First() interface{} {
 		return arr.Index(0).Interface()
 	}
 	return nil
-}
-
-func GetNetStatPorts(state SocketState, processName string) []*SockTabEntry {
-	tabs := []*SockTabEntry{}
-
-	cmd := exec.Command("netstat", "-a", "-n", "-o", "-p", "tcp")
-	out, err := cmd.Output()
-	if err != nil {
-		log.Println(err)
-		return nil
-	}
-
-	procs, err := gops.Processes()
-	if err != nil {
-		log.Println(err)
-		return nil
-	}
-
-	processList := NewList(procs)
-	processList = processList.Filter(func(p interface{}) bool {
-		proc := p.(gops.Process)
-		a := strings.ToLower(proc.Executable())
-		b := strings.ToLower(processName)
-		return strings.Contains(a, b)
-	})
-
-	rr := regexp.MustCompile(`\r\n`)
-	rows := rr.Split(string(out), -1)
-
-	for _, row := range rows {
-		if row == "" {
-			continue
-		}
-
-		tr := regexp.MustCompile(`\s+`)
-		tokens := tr.Split(row, -1)
-		if len(tokens) <= 5 {
-			continue
-		}
-
-		pid, err := strconv.Atoi(tokens[5])
-		if err != nil {
-			continue
-		}
-
-		proc := processList.Filter(func(p interface{}) bool {
-			proc := p.(gops.Process)
-			return proc.Pid() == pid
-		}).First()
-
-		if tokens[1] == "TCP" && tokens[4] == string(state) && proc != nil {
-			localAddress := tokens[2]
-			tabs = append(tabs, &SockTabEntry{
-				Process:      proc.(gops.Process),
-				LocalAddress: localAddress,
-			})
-		}
-	}
-
-	return tabs
 }
