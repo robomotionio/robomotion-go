68e3a7d26f9e02c60110839b9300dd707b32ea27
Rohat Uçar <rohat@robomotion.io>
2022-11-09 10:36:30 +0300

    set vault item and get robot info

--- runtime/credential.go
+++ runtime/credential.go
@@ -22,6 +22,37 @@ type credential struct {
 	ItemID  string `json:"itemId"`
 }
 
+func (c *Credential) Set(ctx message.Context, data []byte) (map[string]interface{}, error) {
+	if client == nil {
+		return nil, fmt.Errorf("Runtime was not initialized")
+	}
+
+	if c.VaultID != "" && c.ItemID != "" {
+		return client.SetVaultItem(c.VaultID, c.ItemID, data)
+	}
+
+	var (
+		err   error
+		ci    interface{} = c.Name
+		creds credential
+	)
+
+	if c.Scope == "Message" {
+		v := &InVariable{Variable: Variable{Scope: c.Scope, Name: c.Name.(string)}}
+		ci, err = v.Get(ctx)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	err = mapstructure.Decode(ci, &creds)
+	if err != nil {
+		return nil, err
+	}
+
+	return client.SetVaultItem(creds.VaultID, creds.ItemID, data)
+}
+
 func (c *Credential) Get(ctx message.Context) (map[string]interface{}, error) {
 	if client == nil {
 		return nil, fmt.Errorf("Runtime was not initialized")
