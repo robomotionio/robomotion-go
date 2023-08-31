f157204189ed4a338777101edf20fb9781e4626e
Hasan Gürsoy <65349721+hasanhg@users.noreply.github.com>
2023-08-31 16:14:38 +0300

    subscript replacement with dot (#26)
    * subscript replace with dot
    
    * get from data
    
    * convert  path
    
    * msg get

--- message/context.go
+++ message/context.go
@@ -2,6 +2,7 @@ package message
 
 import (
 	"encoding/json"
+	"strings"
 
 	"github.com/tidwall/gjson"
 	"github.com/tidwall/sjson"
@@ -32,7 +33,14 @@ func NewContext(data []byte) Context {
 	}
 }
 
+func convertPath(path string) string {
+	path = strings.ReplaceAll(path, "[", ".")
+	path = strings.ReplaceAll(path, "]", "")
+	return path
+}
+
 func (msg *message) Set(path string, value interface{}) (err error) {
+	path = convertPath(path)
 	msg.data, err = sjson.SetBytes(msg.data, path, value)
 	return
 }
@@ -41,24 +49,29 @@ func (msg *message) GetID() string {
 	return msg.ID
 }
 
+func (msg *message) get(path string) gjson.Result {
+	path = convertPath(path)
+	return gjson.GetBytes(msg.data, path)
+}
+
 func (msg *message) Get(path string) interface{} {
-	return gjson.GetBytes(msg.data, path).Value()
+	return msg.get(path).Value()
 }
 
 func (msg *message) GetString(path string) string {
-	return gjson.GetBytes(msg.data, path).String()
+	return msg.get(path).String()
 }
 
 func (msg *message) GetBool(path string) bool {
-	return gjson.GetBytes(msg.data, path).Bool()
+	return msg.get(path).Bool()
 }
 
 func (msg *message) GetInt(path string) int64 {
-	return gjson.GetBytes(msg.data, path).Int()
+	return msg.get(path).Int()
 }
 
 func (msg *message) GetFloat(path string) float64 {
-	return gjson.GetBytes(msg.data, path).Float()
+	return msg.get(path).Float()
 }
 
 func (msg *message) GetRaw() json.RawMessage {
